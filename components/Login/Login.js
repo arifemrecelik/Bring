a760eec56cabbcaed7aeb2687725fe43fb1831fc
arif.celik <ce.arifemre@gmail.com>
2018-01-09 15:58:42 +0300

    Logo added.

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -1,28 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Image, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 
 export default class Login extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-          <TextInput style={styles.input}/>
-          <TextInput style={styles.input}/>
-      </View>
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+          <Image source={require('../../images/logo.png')}/>
+          <TextInput underlineColorAndroid='transparent' placeholder="username or e-mail"  style={styles.input}/>
+          <TextInput underlineColorAndroid='transparent' placeholder="password" secureTextEntry style={styles.input}/>
+          <TouchableOpacity onPress={this.login} style={styles.loginButton}>
+            <Text style={styles.loginText}>LOGIN</Text>
+          </TouchableOpacity>
+      </KeyboardAvoidingView>
     );
+  }
+
+  login = () => {
+    alert("YEEEY");
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2980b9',
+    backgroundColor: '#e74c3c',
     alignItems: 'center',
     justifyContent: 'center',
   },
   input: {
-    height: 40,
+    height: 60,
     width: 300,
-    padding: 20,
-    backgroundColor: 'rgba(255,255,255,0.2)'
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    fontSize: 20,
+    backgroundColor: 'rgba(255,255,255,0.1)'
+  },
+  loginButton: {
+    height: 60,
+    width: 300,
+    paddingVertical: 15,
+    backgroundColor: '#c0392b'
+  },
+  loginText: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+    fontSize: 20
   }
 });
